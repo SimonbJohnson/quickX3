732827a8d3534d0d3ec524d7efe3ec5e05a99b97
SJohnson <simonbjohnson@gmail.com>
2019-02-19 12:39:57 +0000

    updating models

--- hxldash/models.py
+++ hxldash/models.py
@@ -9,10 +9,6 @@ class BiteConfig(models.Model):
 	variety = models.CharField(max_length=200)
 	dataSource =  models.CharField(max_length=200)
 	biteID =  models.CharField(max_length=200)
-	editpassword = models.CharField(max_length=200)
-	viewpassword = models.CharField(max_length=200)
-	user = models.CharField(max_length=200)
-	org = models.CharField(max_length=200)
 
 class FilterConfig(models.Model):
 	text = models.CharField(max_length=20)
@@ -21,8 +17,12 @@ class FilterConfig(models.Model):
 class DashboardConfig(models.Model):
 
 	title =  models.CharField(max_length=200)
-	subtext = models.CharField(max_length=200)
+	subtext = models.CharField(max_length=2000)
 	grid = models.CharField(max_length=10)
 	bites = models.ManyToManyField(BiteConfig)
 	filters = models.ManyToManyField(FilterConfig)
+	editpassword = models.CharField(max_length=200)
+	viewpassword = models.CharField(max_length=200)
+	user = models.CharField(max_length=200)
+	org = models.CharField(max_length=200)
 
