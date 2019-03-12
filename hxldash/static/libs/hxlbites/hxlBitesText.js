b25cd3a5c4cd60460ddc3e969d4d053f11d0084f
SJohnson <simonbjohnson@gmail.com>
2019-03-12 17:46:19 +0000

    adding capacity bite

--- hxldash/static/libs/hxlbites/hxlBitesText.js
+++ hxldash/static/libs/hxlbites/hxlBitesText.js
@@ -113,7 +113,7 @@ hxlBites._textBites = [
 'id':'text0012',	
 'type':'text',
 'subType':'topline figure',
-'ingredients':[{'name':'value','tags':['#affected','#population','#reached','#value']}],
+'ingredients':[{'name':'value','tags':['#affected','#population','#reached','#value','#capacity']}],
 'criteria':['value > 0'],
 'variables': ['sum(value)','header(value)'],
 'phrase': '{2}:{1}',
