081ae577a3ff0ced5be16dc6a0f6af71759295fb
SJohnson <simonbjohnson@gmail.com>
2020-04-11 12:20:00 +0100

    creating mange.py command to populate database with example dashboards

--- hxldash/management/commands/populate_database.py
+++ hxldash/management/commands/populate_database.py
@@ -1,8 +1,14 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.utils import timezone
+from hxldash.views import saveConfig
+import json
 
 class Command(BaseCommand):
 	help = 'Populate the database with some initial dashboards'
 
 	def handle(self, *args, **kwargs):
-		
+		with open('hxldash/management/commands/data.json') as f:
+  			configs = json.load(f)
+  			for config in configs:
+  				saveConfig(config,'','','Demo data','Demo data')
+		self.stdout.write("Success")
